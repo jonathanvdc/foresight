03496d1e94f1a035b38352f6dc9d46408a3abf89
Jonathan Van der Cruysse <jonathan.vdc@outlook.com>
2025-03-26 14:09:49 -0400

    Introduce arithmetic saturation tests

---
+++ src/test/fixpoint/eqsat/saturation/SaturateArithTest.scala
@@ -0,0 +1,172 @@
+package fixpoint.eqsat.saturation
+
+import fixpoint.eqsat.rewriting.Rule
+import fixpoint.eqsat.{EGraph, ENode, MixedTree, Slot}
+import fixpoint.eqsat.rewriting.patterns.{Pattern, PatternMatch, SlotVar}
+import org.junit.Test
+
+class SaturateArithTest {
+  // Define a language for arithmetic expressions involving variable references, addition, multiplication, and integer
+  // literals.
+  sealed trait Arith
+  object Var extends Arith {
+    def apply(slot: Slot): ENode[Arith] = ENode(this, Seq.empty, Seq(slot), Seq.empty)
+    def apply(slotVar: SlotVar): Pattern.Node[Arith] = Pattern.Node(this, Seq.empty, Seq(slotVar), Seq.empty)
+  }
+  object Add extends Arith {
+    def apply(lhs: Pattern[Arith], rhs: Pattern[Arith]): Pattern.Node[Arith] = {
+      Pattern.Node(this, Seq.empty, Seq.empty, Seq(lhs, rhs))
+    }
+    def apply[A](lhs: MixedTree[Arith, A], rhs: MixedTree[Arith, A]): MixedTree[Arith, A] =
+      MixedTree.Node[Arith, A](this, Seq.empty, Seq.empty, Seq(lhs, rhs))
+  }
+  object Mul extends Arith {
+    def apply(lhs: Pattern[Arith], rhs: Pattern[Arith]): Pattern.Node[Arith] = {
+      Pattern.Node(this, Seq.empty, Seq.empty, Seq(lhs, rhs))
+    }
+    def apply[A](lhs: MixedTree[Arith, A], rhs: MixedTree[Arith, A]): MixedTree[Arith, A] =
+      MixedTree.Node[Arith, A](this, Seq.empty, Seq.empty, Seq(lhs, rhs))
+  }
+  final case class Number(value: Int) extends Arith
+  object Number {
+    implicit def toPattern(number: Number): Pattern[Arith] = Pattern.Node(number, Seq.empty, Seq.empty, Seq.empty)
+    implicit def toMixedTree[A](number: Number): MixedTree[Arith, A] = MixedTree.Node(number, Seq.empty, Seq.empty, Seq.empty)
+    implicit def toENode(number: Number): ENode[Arith] = ENode(number, Seq.empty, Seq.empty, Seq.empty)
+  }
+
+  // Define a set of rules for transforming arithmetic expressions.
+  object Rules {
+    def all: Seq[Rule[Arith, PatternMatch[Arith], EGraph[Arith]]] = Seq(
+      addCommutativity,
+      addAssociativity1,
+      addAssociativity2,
+      mulCommutativity,
+      mulAssociativity1,
+      mulAssociativity2,
+      distributivity1,
+      distributivity2)
+
+    val addCommutativity: Rule[Arith, PatternMatch[Arith], EGraph[Arith]] = {
+      val x = Pattern.Var.fresh[Arith]()
+      val y = Pattern.Var.fresh[Arith]()
+      Rule(
+        "x + y = y + x",
+        Add(x, y).toSearcher,
+        Add(y, x).toApplier)
+    }
+
+    val addAssociativity1: Rule[Arith, PatternMatch[Arith], EGraph[Arith]] = {
+      val x = Pattern.Var.fresh[Arith]()
+      val y = Pattern.Var.fresh[Arith]()
+      val z = Pattern.Var.fresh[Arith]()
+      Rule(
+        "(x + y) + z = x + (y + z)",
+        Add(Add(x, y), z).toSearcher,
+        Add(x, Add(y, z)).toApplier)
+    }
+
+    val addAssociativity2: Rule[Arith, PatternMatch[Arith], EGraph[Arith]] = {
+      val x = Pattern.Var.fresh[Arith]()
+      val y = Pattern.Var.fresh[Arith]()
+      val z = Pattern.Var.fresh[Arith]()
+      Rule(
+        "x + (y + z) = (x + y) + z",
+        Add(x, Add(y, z)).toSearcher,
+        Add(Add(x, y), z).toApplier)
+    }
+
+    val mulCommutativity: Rule[Arith, PatternMatch[Arith], EGraph[Arith]] = {
+      val x = Pattern.Var.fresh[Arith]()
+      val y = Pattern.Var.fresh[Arith]()
+      Rule(
+        "x * y = y * x",
+        Mul(x, y).toSearcher,
+        Mul(y, x).toApplier)
+    }
+
+    val mulAssociativity1: Rule[Arith, PatternMatch[Arith], EGraph[Arith]] = {
+      val x = Pattern.Var.fresh[Arith]()
+      val y = Pattern.Var.fresh[Arith]()
+      val z = Pattern.Var.fresh[Arith]()
+      Rule(
+        "(x * y) * z = x * (y * z)",
+        Mul(Mul(x, y), z).toSearcher,
+        Mul(x, Mul(y, z)).toApplier)
+    }
+
+    val mulAssociativity2: Rule[Arith, PatternMatch[Arith], EGraph[Arith]] = {
+      val x = Pattern.Var.fresh[Arith]()
+      val y = Pattern.Var.fresh[Arith]()
+      val z = Pattern.Var.fresh[Arith]()
+      Rule(
+        "x * (y * z) = (x * y) * z",
+        Mul(x, Mul(y, z)).toSearcher,
+        Mul(Mul(x, y), z).toApplier)
+    }
+
+    val distributivity1: Rule[Arith, PatternMatch[Arith], EGraph[Arith]] = {
+      val x = Pattern.Var.fresh[Arith]()
+      val y = Pattern.Var.fresh[Arith]()
+      val z = Pattern.Var.fresh[Arith]()
+      Rule(
+        "x * (y + z) = x * y + x * z",
+        Mul(x, Add(y, z)).toSearcher,
+        Add(Mul(x, y), Mul(x, z)).toApplier)
+    }
+
+    val distributivity2: Rule[Arith, PatternMatch[Arith], EGraph[Arith]] = {
+      val a = Pattern.Var.fresh[Arith]()
+      val b = Pattern.Var.fresh[Arith]()
+      val c = Pattern.Var.fresh[Arith]()
+      Rule(
+        "a * b + a * c = a * (b + c)",
+        Add(Mul(a, b), Mul(a, c)).toSearcher,
+        Mul(a, Add(b, c)).toApplier)
+    }
+  }
+
+  /**
+   * Test that addition is commutative.
+   */
+  @Test
+  def additionIsAssociative(): Unit = {
+    // x + y = y + x
+    val x = Var(Slot.fresh())
+    val y = Var(Slot.fresh())
+    val xPlusY = Add(x, y)
+    val yPlusX = Add(y, x)
+
+    val egraph = EGraph.empty[Arith]
+    val (c1, egraph2) = egraph.add(xPlusY)
+    val (c2, egraph3) = egraph2.add(yPlusX)
+
+    assert(!egraph3.areSame(c1, c2))
+
+    val strategy = MaximalRuleApplication(Rules.all).untilFixpoint
+    val Some(egraph4) = strategy(egraph3)
+
+    assert(egraph4.areSame(c1, c2))
+  }
+
+  @Test
+  def additionIsAssociative2(): Unit = {
+    // (x + y) * (x + y) = (x + y) * (y + x)
+    val x = Var(Slot.fresh())
+    val y = Var(Slot.fresh())
+    val xPlusY = Add(x, y)
+    val yPlusX = Add(y, x)
+    val xPlusYTimesXPlusY = Mul(xPlusY, xPlusY)
+    val xPlusYTimesYPlusX = Mul(xPlusY, yPlusX)
+
+    val egraph = EGraph.empty[Arith]
+    val (c1, egraph2) = egraph.add(xPlusYTimesXPlusY)
+    val (c2, egraph3) = egraph2.add(xPlusYTimesYPlusX)
+
+    assert(!egraph3.areSame(c1, c2))
+
+    val strategy = MaximalRuleApplication(Rules.all)//.untilFixpoint
+    val Some(egraph4) = strategy(egraph3)
+
+    assert(egraph4.areSame(c1, c2))
+  }
+}
