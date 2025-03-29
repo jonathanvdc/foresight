dc5ec2ba98f96497d1699faade2104b6cbb89762
Jonathan Van der Cruysse <jonathan.vdc@outlook.com>
2025-03-29 16:22:21 -0400

    Add additional LIAR arithmetic tests

--- src/test/fixpoint/eqsat/integration/liar/ArithRuleTests.scala
+++ src/test/fixpoint/eqsat/integration/liar/ArithRuleTests.scala
@@ -76,4 +76,93 @@ class ArithRuleTests {
 
     assert(egraph3.areSame(egraph3.find(zero).get, egraph3.find(product).get))
   }
+
+  @Test
+  def introduceAddZero(): Unit = {
+    val egraph = EGraph.empty[ArrayIR]
+
+    val x = Var(Slot.fresh(), DoubleType.toTree)
+    val sum = x
+
+    val (_, egraph2) = egraph.add(sum)
+
+    val egraph3 = strategy(1)(egraph2).get
+
+    assert(egraph3.areSame(egraph3.find(sum).get, egraph3.find(Add(x, ConstDouble(0.0).toTree)).get))
+  }
+
+  @Test
+  def introduceMulOneLeft(): Unit = {
+    val egraph = EGraph.empty[ArrayIR]
+
+    val x = Var(Slot.fresh(), DoubleType.toTree)
+    val product = x
+
+    val (_, egraph2) = egraph.add(product)
+
+    val egraph3 = strategy(1)(egraph2).get
+
+    assert(egraph3.areSame(egraph3.find(product).get, egraph3.find(Mul(ConstDouble(1.0).toTree, x)).get))
+  }
+
+  @Test
+  def introduceMulOneRight(): Unit = {
+    val egraph = EGraph.empty[ArrayIR]
+
+    val x = Var(Slot.fresh(), Int32Type.toTree)
+    val product = x
+
+    val (_, egraph2) = egraph.add(product)
+
+    val egraph3 = strategy(1)(egraph2).get
+
+    assert(egraph3.areSame(egraph3.find(product).get, egraph3.find(Mul(x, ConstInt32(1).toTree)).get))
+  }
+
+  @Test
+  def mulCommutes(): Unit = {
+    val egraph = EGraph.empty[ArrayIR]
+
+    val x = Var(Slot.fresh(), Int32Type.toTree)
+    val y = Var(Slot.fresh(), Int32Type.toTree)
+    val product = Mul(x, y)
+
+    val (_, egraph2) = egraph.add(product)
+
+    val egraph3 = strategy(1)(egraph2).get
+
+    assert(egraph3.areSame(egraph3.find(product).get, egraph3.find(Mul(y, x)).get))
+  }
+
+  @Test
+  def mulAssociates(): Unit = {
+    val egraph = EGraph.empty[ArrayIR]
+
+    val x = Var(Slot.fresh(), Int32Type.toTree)
+    val y = Var(Slot.fresh(), Int32Type.toTree)
+    val z = Var(Slot.fresh(), Int32Type.toTree)
+    val product = Mul(Mul(x, y), z)
+
+    val (_, egraph2) = egraph.add(product)
+
+    val egraph3 = strategy(1)(egraph2).get
+
+    assert(egraph3.areSame(egraph3.find(product).get, egraph3.find(Mul(x, Mul(y, z))).get))
+  }
+
+  @Test
+  def mulAssociates2(): Unit = {
+    val egraph = EGraph.empty[ArrayIR]
+
+    val x = Var(Slot.fresh(), Int32Type.toTree)
+    val y = Var(Slot.fresh(), Int32Type.toTree)
+    val z = Var(Slot.fresh(), Int32Type.toTree)
+    val product = Mul(x, Mul(y, z))
+
+    val (_, egraph2) = egraph.add(product)
+
+    val egraph3 = strategy(1)(egraph2).get
+
+    assert(egraph3.areSame(egraph3.find(product).get, egraph3.find(Mul(Mul(x, y), z)).get))
+  }
 }
