79e0b372000afa8ff922d018718ac11edfaf8567
Jonathan Van der Cruysse <jonathan.vdc@outlook.com>
2025-03-28 22:41:53 -0400

    Define Extractor.apply overload for trees

--- src/main/fixpoint/eqsat/extraction/Extractor.scala
+++ src/main/fixpoint/eqsat/extraction/Extractor.scala
@@ -1,6 +1,6 @@
 package fixpoint.eqsat.extraction
 
-import fixpoint.eqsat.{EClassCall, EGraph, EGraphLike, Tree}
+import fixpoint.eqsat.{EClassCall, EGraph, EGraphLike, MixedTree, Tree}
 
 /**
  * An extractor that extract expression trees from e-class calls.
@@ -15,4 +15,15 @@ trait Extractor[NodeT, Repr <: EGraphLike[NodeT, Repr] with EGraph[NodeT]] {
    * @return The extracted tree.
    */
   def apply(call: EClassCall, egraph: Repr): Tree[NodeT]
+
+  /**
+   * Extracts an expression tree from a mixed tree.
+   * @param tree The mixed tree to extract a tree from.
+   * @param egraph The e-graph containing the mixed tree.
+   * @return The extracted tree.
+   */
+  final def apply(tree: MixedTree[NodeT, EClassCall], egraph: Repr): Tree[NodeT] = {
+    val (call, newGraph) = egraph.add(tree)
+    apply(call, newGraph)
+  }
 }
