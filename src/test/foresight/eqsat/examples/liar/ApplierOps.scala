04dcc56629f2974bbd62b7ef78d06d4a5cfa38c2
Jonathan Van der Cruysse <jonathan.vdc@outlook.com>
2025-04-13 14:16:49 -0400

    Add typeChecked method to PatternApplierOps for type validation before e-graph addition

--- src/test/foresight/eqsat/examples/liar/ApplierOps.scala
+++ src/test/foresight/eqsat/examples/liar/ApplierOps.scala
@@ -1,15 +1,23 @@
 package foresight.eqsat.examples.liar
 
 import foresight.eqsat.{EClassCall, EGraph, EGraphLike, MixedTree, Slot, Tree}
-import foresight.eqsat.commands.Command
+import foresight.eqsat.commands.{Command, EClassSymbol}
 import foresight.eqsat.extraction.ExtractionAnalysis
 import foresight.eqsat.metadata.EGraphWithMetadata
 import foresight.eqsat.rewriting.Applier
-import foresight.eqsat.rewriting.patterns.{Pattern, PatternMatch}
+import foresight.eqsat.rewriting.patterns.{Pattern, PatternApplier, PatternMatch}
 
 object ApplierOps {
   implicit class ApplierOfPatternMatchOps[EGraphT <: EGraphLike[ArrayIR, EGraphT] with EGraph[ArrayIR]](private val applier: Applier[ArrayIR, PatternMatch[ArrayIR], EGraphWithMetadata[ArrayIR, EGraphT]]) extends AnyVal {
 
+    /**
+     * Substitutes a variable in a pattern match with another variable.
+     * @param source The source pattern variable to perform the substitution on.
+     * @param from The variable to be substituted.
+     * @param to The variable to substitute with.
+     * @param destination A variable to store the substituted result in within the pattern match.
+     * @return An applier that performs the substitution.
+     */
     def substitute(source: Pattern.Var[ArrayIR],
                    from: Slot,
                    to: Pattern.Var[ArrayIR],
@@ -29,6 +37,27 @@ object ApplierOps {
           val substituted = subst(extracted)
           val newMatch = m.copy(varMapping = m.varMapping + (destination -> substituted))
           applier.apply(newMatch, egraph)
+        }
+      }
+    }
+  }
+
+  implicit class PatternApplierOps[EGraphT <: EGraphLike[ArrayIR, EGraphT] with EGraph[ArrayIR]](private val applier: PatternApplier[ArrayIR, EGraphWithMetadata[ArrayIR, EGraphT]]) extends AnyVal {
+    private def inferType(tree: MixedTree[ArrayIR, EClassCall], egraph: EGraphWithMetadata[ArrayIR, EGraphT]): MixedTree[ArrayIR, EClassCall] = {
+      val (call, newEGraph) = egraph.add(tree)
+      TypeInferenceAnalysis.get(newEGraph)(call, newEGraph)
+    }
+
+    /**
+     * Checks the types of the instantiated pattern before adding it to the e-graph.
+     * @return An applier that checks the types of the instantiated pattern.
+     */
+    def typeChecked: Applier[ArrayIR, PatternMatch[ArrayIR], EGraphWithMetadata[ArrayIR, EGraphT]] = {
+      new Applier[ArrayIR, PatternMatch[ArrayIR], EGraphWithMetadata[ArrayIR, EGraphT]] {
+        override def apply(m: PatternMatch[ArrayIR], egraph: EGraphWithMetadata[ArrayIR, EGraphT]): Command[ArrayIR] = {
+          val tree = applier.instantiate(m)
+          inferType(tree.mapCalls(_.asInstanceOf[EClassSymbol.Real].call), egraph)
+          Command.addEquivalentTree(EClassSymbol.real(m.root), tree)
         }
       }
     }
