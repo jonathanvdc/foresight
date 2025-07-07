2f2a9222a3223121d266a05d67fc6c00072c291f
Jonathan Van der Cruysse <jonathan.vdc@outlook.com>
2025-07-07 17:31:56 -0400

    Add documentation and factory method for caching in SearchAndApply trait

--- src/main/foresight/eqsat/saturation/SearchAndApply.scala
+++ src/main/foresight/eqsat/saturation/SearchAndApply.scala
@@ -5,18 +5,52 @@ import foresight.eqsat.parallel.ParallelMap
 import foresight.eqsat.rewriting.{PortableMatch, Rule}
 import foresight.eqsat.{EGraph, EGraphLike}
 
+/**
+ * A strategy that searches for matches of a set of rules in an e-graph and applies them.
+ *
+ * @tparam RuleT The type of the rules to search and apply.
+ * @tparam EGraphT The type of the e-graph.
+ * @tparam MatchT The type of the matches produced by the rules.
+ */
 trait SearchAndApply[RuleT <: Rule[_, MatchT, _], EGraphT <: EGraphLike[_, EGraphT] with EGraph[_], MatchT] {
+  /**
+   * Searches for matches of the given rules in the e-graph.
+   *
+   * @param rules The rules to search for matches.
+   * @param egraph The e-graph to search in.
+   * @param parallelize A parallelization strategy for searching.
+   * @return A map from rule names to sequences of matches found for each rule.
+   */
   def search(rules: Seq[RuleT],
              egraph: EGraphT,
              parallelize: ParallelMap): Map[String, Seq[MatchT]]
 
+  /**
+   * Applies the matches found for the given rules to the e-graph.
+   * @param rules The rules whose matches are to be applied.
+   * @param matches A map from rule names to sequences of matches found for each rule.
+   * @param egraph The e-graph to which the matches are applied.
+   * @param parallelize A parallelization strategy for applying the matches.
+   * @return An updated e-graph with the matches applied, or None if no matches were applied.
+   */
   def apply(rules: Seq[RuleT],
             matches: Map[String, Seq[MatchT]],
             egraph: EGraphT,
             parallelize: ParallelMap): Option[EGraphT]
 }
 
+/**
+ * A companion object for the [[SearchAndApply]] trait that provides a factory method for creating a
+ * [[SearchAndApply]] instance with caching capabilities.
+ */
 object SearchAndApply {
+  /**
+   * Creates a [[SearchAndApply]] instance that caches the matches that have already been applied to the e-graph.
+   * @tparam NodeT The type of the nodes in the e-graph.
+   * @tparam EGraphT The type of the e-graph.
+   * @tparam MatchT The type of the matches produced by the rules.
+   * @return A [[SearchAndApply]] instance that caches matches.
+   */
   def withCaching[NodeT,
                   EGraphT <: EGraphLike[NodeT, EGraphT] with EGraph[NodeT],
                   MatchT <: PortableMatch[NodeT, MatchT]]: SearchAndApply[Rule[NodeT, MatchT, EGraphT], EGraphWithRecordedApplications[NodeT, EGraphT, MatchT], MatchT] = {
