fd1eb347dfff0a131f20287bc79cae5381d60c6a
jonathanvdc <jonathan.vdc@outlook.com>
2025-07-15 16:26:08 -0400

    Enhance Rebase strategy to support EGraphWithRoot and simplify initial data handling

--- src/main/foresight/eqsat/saturation/Rebase.scala
+++ src/main/foresight/eqsat/saturation/Rebase.scala
@@ -2,7 +2,7 @@ package foresight.eqsat.saturation
 
 import foresight.eqsat.extraction.Extractor
 import foresight.eqsat.parallel.ParallelMap
-import foresight.eqsat.{EClassCall, EGraph, EGraphLike}
+import foresight.eqsat.{EClassCall, EGraph, EGraphLike, Tree}
 
 /**
  * A strategy that rebases the e-graph by extracting a tree from the e-graph, subsequently adding that tree to a new
@@ -10,27 +10,48 @@ import foresight.eqsat.{EClassCall, EGraph, EGraphLike}
  *
  * @param extractor The extractor to use for extracting the tree.
  * @param findRoot A function to find the root of the e-graph.
+ * @param withRoot A function to create a new e-graph with the specified root.
  * @tparam NodeT The type of the nodes in the e-graph.
  * @tparam EGraphT The type of the e-graph that the strategy operates on.
  */
 final case class Rebase[NodeT, EGraphT <: EGraphLike[NodeT, EGraphT] with EGraph[NodeT]](extractor: Extractor[NodeT, EGraphT],
-                                                                                         findRoot: EGraphT => EClassCall) extends Strategy[EGraphT, Option[EClassCall]] {
-  /**
-   * The initial data for the strategy's first iteration.
-   */
-  override def initialData: Option[EClassCall] = None
+                                                                                         findRoot: EGraphT => EClassCall,
+                                                                                         withRoot: (EGraphT, EClassCall) => EGraphT) extends Strategy[EGraphT, Unit] {
+  override def initialData: Unit = ()
 
-  override def apply(egraph: EGraphT, data: Option[EClassCall], parallelize: ParallelMap): (Option[EGraphT], Option[EClassCall]) = {
-    val oldRoot = data match {
-      case Some(root) if egraph.contains(root.ref) => root
-      case None => findRoot(egraph)
-    }
+  override def apply(egraph: EGraphT, data: Unit, parallelize: ParallelMap): (Option[EGraphT], Unit) = {
+    val oldRoot = findRoot(egraph)
 
     val tree = extractor(oldRoot, egraph)
 
     val emptyGraph = egraph.emptied
     val (newRoot, newGraph) = emptyGraph.add(tree)
 
-    Some(newGraph) -> Some(newRoot)
+    Some(withRoot(newGraph, newRoot)) -> None
+  }
+}
+
+/**
+ * A companion object for the [[Rebase]] strategy that provides a convenient way to create a [[Rebase]] instance
+ * operating on an [[EGraphWithRoot]].
+ */
+object Rebase {
+  /**
+   * Creates a [[Rebase]] strategy that operates on an [[EGraphWithRoot]] using the provided extractor.
+   * @param extractor The extractor to use for extracting trees from e-class calls in the e-graph.
+   * @tparam NodeT The type of the nodes in the e-graph.
+   * @tparam EGraphT The type of the e-graph that the strategy operates on, which must be a subtype of both [[EGraphLike]] and [[EGraph]].
+   * @return A [[Rebase]] strategy that operates on an [[EGraphWithRoot]].
+   */
+  def apply[NodeT, EGraphT <: EGraphLike[NodeT, EGraphT] with EGraph[NodeT]](extractor: Extractor[NodeT, EGraphT]): Rebase[NodeT, EGraphWithRoot[NodeT, EGraphT]] = {
+    new Rebase(
+      new Extractor[NodeT, EGraphWithRoot[NodeT, EGraphT]] {
+        override def apply(call: EClassCall, egraph: EGraphWithRoot[NodeT, EGraphT]): Tree[NodeT] = {
+          extractor(call, egraph.graph)
+        }
+      },
+      (egraph: EGraphWithRoot[NodeT, EGraphT]) =>
+        egraph.root.getOrElse(throw new IllegalStateException("Root is not set in EGraphWithRoot")),
+      (egraph: EGraphWithRoot[NodeT, EGraphT], root: EClassCall) => egraph.withRoot(root))
   }
 }
