e1b1e2f2fcedf1187b7c5bc7b12baaf294dbfef9
Jonathan Van der Cruysse <jonathan.vdc@outlook.com>
2025-07-23 13:03:44 -0400

    Add withRecording method to TransformAndRebase for e-graph rebasing with recorded applications

--- src/main/foresight/eqsat/saturation/TransformAndRebase.scala
+++ src/main/foresight/eqsat/saturation/TransformAndRebase.scala
@@ -4,6 +4,7 @@ import foresight.eqsat.{EClassCall, EGraph, EGraphLike, Tree}
 import foresight.eqsat.extraction.Extractor
 import foresight.eqsat.metadata.EGraphWithMetadata
 import foresight.eqsat.parallel.ParallelMap
+import foresight.eqsat.rewriting.PortableMatch
 
 /**
  * A strategy that first transforms the e-graph using a given strategy and then rebases the e-graph by extracting a
@@ -168,5 +169,46 @@ object TransformAndRebase {
   def withMetadata[NodeT, EGraphT <: EGraphLike[NodeT, EGraphT] with EGraph[NodeT], Data](transform: Strategy[EGraphWithMetadata[NodeT, EGraphWithRoot[NodeT, EGraphT]], Data],
                                                                                           extractor: Extractor[NodeT, EGraphWithMetadata[NodeT, EGraphWithRoot[NodeT, EGraphT]]]): TransformAndRebase[NodeT, EGraphWithMetadata[NodeT, EGraphWithRoot[NodeT, EGraphT]], Data] = {
     withMetadata(transform, extractor, (x: Tree[NodeT], y: Tree[NodeT]) => x == y)
+  }
+
+  /**
+   * Creates a [[TransformAndRebase]] strategy that operates on an [[EGraphWithRecordedApplications]] of an
+   * [[EGraphWithMetadata]] of an [[EGraphWithRoot]].
+   * A transformation strategy is applied to the e-graph, and then the e-graph is rebased by extracting a tree
+   * from the e-graph and adding it to a new e-graph with a new root. If the transformation does not change
+   * the e-graph, it skips rebasing. If the transformation changes the e-graph, it extracts a new tree
+   * and checks if it is equivalent to the previously extracted tree. If it is, the e-graph is left
+   * unchanged and not rebased; otherwise, it adds the new tree to an empty e-graph and returns the new
+   * e-graph with the new root.
+   *
+   * @param transform The strategy to apply to the e-graph before rebasing.
+   * @param extractor The extractor to use for extracting trees from e-class calls in the e-graph.
+   * @param areEquivalent A function to check if two trees are equivalent. Defaults to structural equality.
+   * @tparam NodeT The type of the nodes in the e-graph.
+   * @tparam EGraphT The type of the e-graph that the strategy operates on, which must be a subtype of both [[EGraphLike]] and [[EGraph]].
+   * @tparam Match The type of matches produced by the transformation strategy.
+   * @tparam Data The type of data that the transformation strategy operates on. This can be used to carry additional
+   *              information during the transformation process.
+   * @return A [[TransformAndRebase]] strategy that operates on an [[EGraphWithRecordedApplications]] of an
+   *         [[EGraphWithMetadata]] of an [[EGraphWithRoot]].
+   */
+  def withRecording[NodeT,
+                    EGraphT <: EGraphLike[NodeT, EGraphT] with EGraph[NodeT],
+                    Match <: PortableMatch[NodeT, Match],
+                    Data](transform: Strategy[EGraphWithRecordedApplications[NodeT, EGraphWithMetadata[NodeT, EGraphWithRoot[NodeT, EGraphT]], Match], Data],
+                          extractor: Extractor[NodeT, EGraphWithMetadata[NodeT, EGraphWithRoot[NodeT, EGraphT]]],
+                          areEquivalent: (Tree[NodeT], Tree[NodeT]) => Boolean): TransformAndRebase[NodeT, EGraphWithRecordedApplications[NodeT, EGraphWithMetadata[NodeT, EGraphWithRoot[NodeT, EGraphT]], Match], Data] = {
+    new TransformAndRebase(
+      transform,
+      new Extractor[NodeT, EGraphWithRecordedApplications[NodeT, EGraphWithMetadata[NodeT, EGraphWithRoot[NodeT, EGraphT]], Match]] {
+        override def apply(call: EClassCall, egraph: EGraphWithRecordedApplications[NodeT, EGraphWithMetadata[NodeT, EGraphWithRoot[NodeT, EGraphT]], Match]): Tree[NodeT] = {
+          extractor(call, egraph.egraph)
+        }
+      },
+      (egraph: EGraphWithRecordedApplications[NodeT, EGraphWithMetadata[NodeT, EGraphWithRoot[NodeT, EGraphT]], Match]) =>
+        egraph.egraph.egraph.root.getOrElse(throw new IllegalStateException("Root is not set in EGraphWithRoot")),
+      (egraph: EGraphWithRecordedApplications[NodeT, EGraphWithMetadata[NodeT, EGraphWithRoot[NodeT, EGraphT]], Match], root: EClassCall) =>
+        egraph.migrateTo(egraph.egraph.migrateTo(egraph.egraph.egraph.withRoot(root))),
+      areEquivalent)
   }
 }
