7826f0d0d95ea999a61b6f1e62d0bbbca8a7c30a
jonathanvdc <jonathan.vdc@outlook.com>
2025-07-28 18:49:23 -0400

    Add isNumbered method to Slot trait for slot type identification

--- src/main/foresight/eqsat/Slot.scala
+++ src/main/foresight/eqsat/Slot.scala
@@ -3,7 +3,18 @@ package foresight.eqsat
 /**
  * A slot in an e-graph. Slots are unique identifiers for variables in the e-graph.
  */
-sealed trait Slot extends Ordered[Slot]
+sealed trait Slot extends Ordered[Slot] {
+  /**
+   * Tests whether this slot is a numbered slot.
+   * Numbered slots are equivalent if they have the same number.
+   * Unique slots are always distinct.
+   * @return True if this slot is a numbered slot, false otherwise.
+   */
+  final def isNumbered: Boolean = this match {
+    case Slot.NumberedSlot(_) => true
+    case _: Slot.UniqueSlot => false
+  }
+}
 
 /**
  * Companion object for the Slot trait.
@@ -13,7 +24,7 @@ object Slot {
    * A numbered slot. All slots with the same number are considered equivalent.
    * @param n The number of the slot.
    */
-  private final case class NumberedSlot(n: Int) extends Slot {
+  final case class NumberedSlot(n: Int) extends Slot {
     override def compare(that: Slot): Int = that match {
       case NumberedSlot(m) => n compare m
       case _: UniqueSlot => -1
@@ -23,7 +34,7 @@ object Slot {
   /**
    * A unique slot. Each unique slot is considered distinct from all other slots.
    */
-  private final class UniqueSlot extends Slot {
+  final class UniqueSlot extends Slot {
     override def compare(that: Slot): Int = that match {
       case _: NumberedSlot => 1
       case _: UniqueSlot => hashCode() compare that.hashCode()
