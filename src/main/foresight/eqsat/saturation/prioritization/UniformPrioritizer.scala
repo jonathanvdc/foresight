3ca06b17a64685b5b1511dd2cd72c463b074911a
Jonathan Van der Cruysse <jonathan.vdc@outlook.com>
2025-07-31 12:05:15 -0400

    Add UniformPrioritizer for uniform match prioritization

---
+++ src/main/foresight/eqsat/saturation/prioritization/UniformPrioritizer.scala
@@ -0,0 +1,23 @@
+package foresight.eqsat.saturation.prioritization
+
+import foresight.eqsat.rewriting.Rule
+
+/**
+ * A prioritizer that assigns a uniform priority to all matches.
+ *
+ * @param maxBatchSize The maximum number of matches to process in a single batch.
+ * @tparam NodeT The type of the nodes in the e-graph.
+ * @tparam RuleT The type of the rule.
+ * @tparam MatchT The type of the matches produced by the rule.
+ */
+final case class UniformPrioritizer[NodeT, RuleT <: Rule[NodeT, MatchT, _], MatchT](maxBatchSize: Int)
+    extends MatchPrioritizer[NodeT, RuleT, MatchT] {
+
+  override def prioritize(matches: Seq[(RuleT, MatchT)]): Seq[(RuleT, MatchT, Double)] = {
+    matches.map { case (rule, match_) => (rule, match_, 1.0) }
+  }
+
+  override def batchSize(matches: Seq[(RuleT, MatchT, Double)]): Int = {
+    math.min(maxBatchSize, matches.size)
+  }
+}
