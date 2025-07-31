ba291234b5677097cdfcd158cb07a599c4f43f12
jonathanvdc <jonathan.vdc@outlook.com>
2025-07-31 16:28:35 -0400

    Define convenience constructors for StochasticRuleApplication

--- src/main/foresight/eqsat/saturation/StochasticRuleApplication.scala
+++ src/main/foresight/eqsat/saturation/StochasticRuleApplication.scala
@@ -29,7 +29,7 @@ final case class StochasticRuleApplication[
   MatchT](rules: Seq[RuleT],
           searchAndApply: SearchAndApply[RuleT, EGraphT, MatchT],
           prioritizer: MatchPrioritizer[NodeT, RuleT, MatchT],
-          random: Random = new Random(0)) extends Strategy[EGraphT, Unit] {
+          random: Random) extends Strategy[EGraphT, Unit] {
 
   /**
    * A map from rule names to the rules themselves. This is used to quickly look up a rule by its name.
@@ -71,5 +71,79 @@ final case class StochasticRuleApplication[
       prioritizedMatches.map { case (rule, matchT, priority) => ((rule, matchT), priority) },
       batchSize,
       random)
+  }
+}
+
+/**
+ * Companion object for [[StochasticRuleApplication]] that provides factory methods to create instances.
+ */
+object StochasticRuleApplication {
+  /**
+   * Creates a new instance of [[StochasticRuleApplication]] with the given rules, search and apply strategy, and
+   * prioritizer.
+   *
+   * @param rules The rules to apply.
+   * @param searchAndApply The search and apply strategy to find and apply matches.
+   * @param prioritizer The prioritizer that determines the priority of matches and the batch size to apply.
+   * @tparam NodeT The type of the nodes in the e-graph.
+   * @tparam RuleT The type of the rules to apply.
+   * @tparam EGraphT The type of the e-graph.
+   * @tparam MatchT The type of the matches produced by the rules.
+   */
+  def apply[
+    NodeT,
+    RuleT <: Rule[NodeT, MatchT, _],
+    EGraphT <: EGraphLike[NodeT, EGraphT] with EGraph[NodeT],
+    MatchT
+  ](
+    rules: Seq[RuleT],
+    searchAndApply: SearchAndApply[RuleT, EGraphT, MatchT],
+    prioritizer: MatchPrioritizer[NodeT, RuleT, MatchT]
+  ): StochasticRuleApplication[NodeT, RuleT, EGraphT, MatchT] = {
+    new StochasticRuleApplication(rules, searchAndApply, prioritizer, new Random(0))
+  }
+
+  /**
+   * Creates a new instance of [[StochasticRuleApplication]] with the given rules and a default search and apply strategy
+   * that does not cache results, along with a prioritizer.
+   *
+   * @param rules The rules to apply.
+   * @param prioritizer The prioritizer that determines the priority of matches and the batch size to apply.
+   * @param random A random number generator used for selecting matches randomly.
+   * @tparam NodeT The type of the nodes in the e-graph.
+   * @tparam EGraphT The type of the e-graph.
+   * @tparam MatchT The type of the matches produced by the rules.
+   */
+  def apply[
+    NodeT,
+    EGraphT <: EGraphLike[NodeT, EGraphT] with EGraph[NodeT],
+    MatchT
+  ](
+    rules: Seq[Rule[NodeT, MatchT, EGraphT]],
+    prioritizer: MatchPrioritizer[NodeT, Rule[NodeT, MatchT, EGraphT], MatchT],
+    random: Random
+  ): StochasticRuleApplication[NodeT, Rule[NodeT, MatchT, EGraphT], EGraphT, MatchT] = {
+    apply(rules, SearchAndApply.withoutCaching[NodeT, EGraphT, MatchT], prioritizer)
+  }
+
+  /**
+   * Creates a new instance of [[StochasticRuleApplication]] with the given rules and a default search and apply strategy
+   * that does not cache results, along with a prioritizer. Uses a default random number generator with seed 0.
+   *
+   * @param rules The rules to apply.
+   * @param prioritizer The prioritizer that determines the priority of matches and the batch size to apply.
+   * @tparam NodeT The type of the nodes in the e-graph.
+   * @tparam EGraphT The type of the e-graph.
+   * @tparam MatchT The type of the matches produced by the rules.
+   */
+  def apply[
+    NodeT,
+    EGraphT <: EGraphLike[NodeT, EGraphT] with EGraph[NodeT],
+    MatchT
+  ](
+    rules: Seq[Rule[NodeT, MatchT, EGraphT]],
+    prioritizer: MatchPrioritizer[NodeT, Rule[NodeT, MatchT, EGraphT], MatchT]
+  ): StochasticRuleApplication[NodeT, Rule[NodeT, MatchT, EGraphT], EGraphT, MatchT] = {
+    apply(rules, prioritizer, new Random(0))
   }
 }
