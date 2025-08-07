32d0ee90dab726f5c1537ce9e0049aac80a325ff
Jonathan Van der Cruysse <jonathan.vdc@outlook.com>
2025-08-07 00:33:31 -0400

    Enhance documentation for EClassRef

--- src/main/scala/foresight/eqsat/EClassRef.scala
+++ src/main/scala/foresight/eqsat/EClassRef.scala
@@ -1,6 +1,15 @@
 package foresight.eqsat
 
 /**
- * A reference to an e-class. Each e-class can have multiple references.
+ * Represents a reference to an e-class within an e-graph.
+ *
+ * An e-class is a set of equivalent expressions grouped together in an e-graph, a data structure used for representing
+ * and manipulating equivalence relations between expressions.
+ *
+ * When a new e-class is created, a unique `EClassRef` is generated for it. Over time, as equivalence relations are
+ * discovered and e-classes are merged, multiple `EClassRef` instances may refer to the same underlying e-class.
+ *
+ * To resolve ambiguity and obtain a single, canonical reference for an e-class, use
+ * [[EGraphLike.canonicalize]].
  */
 final class EClassRef
