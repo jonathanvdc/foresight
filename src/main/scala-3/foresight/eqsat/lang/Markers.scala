778384365ab47e002689cb9f5a0c9cc018ef0453
jonathanvdc <jonathan.vdc@outlook.com>
2025-08-18 12:12:13 -0400

    Define implicit conversions for Def[Slot] and Use[Slot]

--- src/main/scala-3/foresight/eqsat/lang/Markers.scala
+++ src/main/scala-3/foresight/eqsat/lang/Markers.scala
@@ -1,5 +1,16 @@
 package foresight.eqsat.lang
 
+import foresight.eqsat.Slot
+import scala.language.implicitConversions
+import scala.Conversion
+
 // Mark Slot fields: use vs binder
 final case class Use[A](value: A) extends AnyVal
+object Use:
+  given Conversion[Slot, Use[Slot]] with
+    def apply(s: Slot): Use[Slot] = Use(s)
+
 final case class Def[A](value: A) extends AnyVal
+object Def:
+  given Conversion[Slot, Def[Slot]] with
+    def apply(s: Slot): Def[Slot] = Def(s)
