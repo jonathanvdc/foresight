99c378783d81fed44d5a333d74459ffca0ec5474
Abd-El-Aziz Zayed <azayed01@gmail.com>
2025-09-01 12:05:53 -0400

    Fast bench

---
+++ src/test/scala-3/foresight/eqsat/examples/Main.scala
@@ -0,0 +1,154 @@
+package foresight.eqsat.examples
+
+import foresight.eqsat.examples.mm.*
+import foresight.eqsat.examples.poly.*
+import foresight.eqsat.lang.*
+import foresight.eqsat.parallel.ParallelMap
+import foresight.eqsat.saturation.{MaximalRuleApplication, Strategy}
+import foresight.eqsat.{EClassCall, EGraph}
+
+object Main {
+
+
+  val map1 = ParallelMap.default
+  val map2 = ParallelMap.sequential
+//  val map3 = ParallelMap.fixedThreadParallel(
+
+  final case class DimAndCost(nrows: Int, ncols: Int, cost: Int)
+  given Ordering[DimAndCost] = Ordering.by(_.cost)
+
+  def nmm(n: Int): LinalgExpr = {
+    if (n == 0) {
+      mm.Mat(10, 10)
+    } else {
+      mm.Mul(nmm(n - 1), Mat(10, 10))
+    }
+  }
+
+  def testNmmBench(n: Int): Unit = {
+    val L: Language[LinalgExpr] = summon[Language[LinalgExpr]]
+    val R: mm.Rules = mm.Rules()(using L)
+    type LinalgRule = R.LinalgRule
+
+    val simpleStrategy: Strategy[LinalgIR, EGraph[LinalgIR], Unit] = MaximalRuleApplication(R.all).repeatUntilStable
+
+    val costFunction: LanguageCostFunction[LinalgExpr, DimAndCost] = new LanguageCostFunction[LinalgExpr, DimAndCost]() {
+      override def apply(expr: LinalgExpr): DimAndCost = {
+        expr match {
+          case mm.Mat(rows, cols) => DimAndCost(rows, cols, 0)
+          case mm.Fact(dimAndCost: DimAndCost) => dimAndCost
+          case mm.Mul(lhs, rhs) =>
+            val DimAndCost(lrows, lcols, lcost) = apply(lhs)
+            val DimAndCost(rrows, rcols, rcost) = apply(rhs)
+            if (lcols != rrows) {
+              throw new RuntimeException(s"Dimension mismatch: $lcols != $rrows")
+            } else {
+              DimAndCost(lrows, rcols, lrows * lcols * rcols + lcost + rcost)
+            }
+          case _ => DimAndCost(-1, -1, 0)
+        }
+      }
+    }
+
+    val expr = nmm(n)
+
+    val (root, egraph) = L.toEGraph(expr)
+    val egraph2 = simpleStrategy(egraph, map2).get
+
+    val extracted = egraph2.extract(root, costFunction)
+  }
+
+  def benchNmm(n: Int): Unit = {
+    val time = 60_000_000_000L
+    val start = System.nanoTime()
+    var times: List[Long] = List()
+    var iterations = 0
+    while (System.nanoTime() - start < time) {
+      val testStart = System.nanoTime()
+      testNmmBench(n)
+      val testEnd = System.nanoTime()
+      val duration = testEnd - testStart
+      //      println(s"Iteration $iterations took $duration ms")
+      times = duration :: times
+      iterations += 1
+    }
+    println(s"Completed $iterations iterations in 60 seconds")
+    val avgTime = if (times.nonEmpty) times.sum / times.length else 0
+    println(s"Average time per iteration: ${avgTime / 1e6} ms")
+  }
+
+  def benchMM(): Unit = {
+    val n = List(3, 5, 10, 20, 40, 80)
+    for (i <- n) {
+      println(s"Benchmarking n=$i")
+      benchNmm(i)
+    }
+  }
+
+  def testPoly5Bench(): Unit = {
+    val L: Language[ArithExpr] = summon[Language[ArithExpr]]
+    val R: poly.Rules = poly.Rules()(using L)
+    type ArithRule = R.ArithRule
+
+    val simpleStrategy: Strategy[ArithIR, EGraph[ArithIR], Unit] = MaximalRuleApplication(R.all).repeatUntilStable
+    val costFunction: LanguageCostFunction[ArithExpr, Int] = new LanguageCostFunction[ArithExpr, Int]() {
+      override def apply(expr: ArithExpr): Int = {
+        expr match {
+          case poly.Add(lhs, rhs) => apply(lhs) + apply(rhs) + 10
+          case poly.Mul(lhs, rhs) => apply(lhs) + apply(rhs) + 100
+          case poly.Pow(lhs, rhs) => apply(lhs) + apply(rhs) + 1000
+          case poly.Fact(cost: Int) => cost
+          case _ => 1
+        }
+      }
+    }
+
+    // polynomial of degree 5: ax^5 + bx^4 + cx^3 + dx^2 + ex + f
+    val c0 = Zero // 0
+    val c1 = Succ(c0) // 1
+    val c2 = Succ(c1) // 2
+    val c3 = Succ(c2) // 3
+    val c4 = Succ(c3) // 4
+    val c5 = Succ(c4) // 5
+
+    val x = Var("x")
+    val a = Var("a")
+    val b = Var("b")
+    val c = Var("c")
+    val d = Var("d")
+    val e = Var("e")
+    val f = Var("f")
+
+    val poly5 = a * (x ** c5) + b * (x ** c4) + c * (x ** c3) + d * (x ** c2) + e * x + f
+    val poly5Simplified = f + x * (e + x * (d + x * (c + x * (b + a * x))))
+
+    val (root, egraph) = L.toEGraph(poly5)
+    val egraph2 = simpleStrategy(egraph, map2).get
+
+    val extracted = egraph2.extract(root, costFunction)
+  }
+
+  def benchPoly5(): Unit = {
+    val time = 60_000_000_000L
+    val start = System.nanoTime()
+    var times: List[Long] = List()
+    var iterations = 0
+    while (System.nanoTime() - start < time) {
+      val testStart = System.nanoTime()
+      testPoly5Bench()
+      val testEnd = System.nanoTime()
+      val duration = testEnd - testStart
+      //      println(s"Iteration $iterations took $duration ms")
+      times = duration :: times
+      iterations += 1
+    }
+    println(s"Completed $iterations iterations in 60 seconds")
+    val avgTime = if (times.nonEmpty) times.sum / times.length else 0
+    println(s"Average time per iteration: ${avgTime / 1e6} ms")
+  }
+
+  def main(args: Array[String]): Unit = {
+    benchPoly5()
+    benchMM()
+  }
+}
