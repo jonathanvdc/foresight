ff0a4a31b6dbb3e1a0bbbf4a716d550c683edf5e
jonathanvdc <jonathan.vdc@outlook.com>
2025-09-29 14:49:10 -0400

    Implement mutable union-find data structure using a hash map for parent pointers

---
+++ foresight/src/main/scala/foresight/eqsat/hashCons/mutable/SlottedUnionFind.scala
@@ -0,0 +1,21 @@
+package foresight.eqsat.hashCons.mutable
+
+import foresight.eqsat.{EClassCall, EClassRef}
+import foresight.eqsat.hashCons.AbstractMutableSlottedUnionFind
+
+import scala.collection.mutable
+
+/**
+ * A mutable union-find data structure that uses a hash map to store parent pointers.
+  *
+  * Write operations to this implementation is not thread-safe.
+  */
+private[hashCons] final class SlottedUnionFind extends AbstractMutableSlottedUnionFind {
+  private val parents: mutable.HashMap[EClassRef, EClassCall] = mutable.HashMap.empty
+
+  override def update(key: EClassRef, value: EClassCall): Unit = {
+    parents.update(key, value)
+  }
+
+  override protected def getParentOrNull(ref: EClassRef): EClassCall = parents.getOrElse(ref, null)
+}
