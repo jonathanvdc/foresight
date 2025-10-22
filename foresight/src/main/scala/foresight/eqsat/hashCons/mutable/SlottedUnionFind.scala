7bd8c2e728700776c09366f4ffce4d3837dd996c
jonathanvdc <jonathan.vdc@outlook.com>
2025-10-22 17:16:50 -0400

    Use AtomicReferenceArray for parent pointers in SlottedUnionFind

--- foresight/src/main/scala/foresight/eqsat/hashCons/mutable/SlottedUnionFind.scala
+++ foresight/src/main/scala/foresight/eqsat/hashCons/mutable/SlottedUnionFind.scala
@@ -4,30 +4,45 @@ import foresight.eqsat.collections.{SlotMap, SlotSet}
 import foresight.eqsat.{EClassCall, EClassRef}
 import foresight.eqsat.hashCons.AbstractMutableSlottedUnionFind
 
-import scala.collection.mutable
+import java.util.concurrent.atomic.AtomicReferenceArray
 
 /**
- * A mutable union-find data structure that uses a hash map to store parent pointers.
+ * A mutable union-find data structure that uses an array to store parent pointers.
   *
   * Write operations to this implementation is not thread-safe.
   */
 private[hashCons] final class SlottedUnionFind extends AbstractMutableSlottedUnionFind {
-  private val parents: mutable.ArrayBuffer[EClassCall] = mutable.ArrayBuffer.empty
+  private var parents: AtomicReferenceArray[EClassCall] =
+    new AtomicReferenceArray[EClassCall](16)
+
+  private var currentSize: Int = 0
 
   override def update(key: EClassRef, value: EClassCall): Unit = {
-    parents.update(key.id, value)
+    parents.lazySet(key.id, value)
   }
 
   override protected def getParentOrNull(ref: EClassRef): EClassCall = {
     if (ref.id >= parents.length) null
-    else parents(ref.id)
+    else parents.get(ref.id)
   }
 
-  override def size: Int = parents.size
+  override def size: Int = currentSize
 
   override def add(slots: SlotSet): EClassRef = {
-    val key = EClassRef(size)
-    parents.append(EClassCall(key, SlotMap.identity(slots)))
+    val key = EClassRef(currentSize)
+    if (key.id >= parents.length) {
+      val newLength = parents.length * 2
+      val newParents = new AtomicReferenceArray[EClassCall](newLength)
+      var i = 0
+      while (i < parents.length) {
+        newParents.set(i, parents.get(i))
+        i += 1
+      }
+      parents = newParents
+    }
+
+    currentSize += 1
+    parents.set(key.id, EClassCall(key, SlotMap.identity(slots)))
     key
   }
 }
