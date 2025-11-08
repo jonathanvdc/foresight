5022a8fd654a97cfe1a87bac4759943f3dbc55e4
jonathanvdc <jonathan.vdc@outlook.com>
2025-11-07 21:43:03 -0500

    Refactor IncrementalBenchmarks to extend BenchmarksWithParallelMap

--- benchmarks/src/main/scala-3/foresight/eqsat/examples/incremental/IncrementalBenchmarks.scala
+++ benchmarks/src/main/scala-3/foresight/eqsat/examples/incremental/IncrementalBenchmarks.scala
@@ -6,6 +6,7 @@ import foresight.eqsat.immutable
 import foresight.eqsat.lang.{Language, LanguageCostFunction}
 import foresight.eqsat.parallel.ParallelMap
 import foresight.eqsat.saturation.{MaximalRuleApplication, Strategy}
+import foresight.util.BenchmarksWithParallelMap
 import org.openjdk.jmh.annotations.{Benchmark, BenchmarkMode, Level, Mode, OutputTimeUnit, Param, Scope, Setup, State}
 
 import java.util.concurrent.TimeUnit
@@ -13,7 +14,7 @@ import java.util.concurrent.TimeUnit
 @State(Scope.Thread)
 @BenchmarkMode(Array(Mode.AverageTime))
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
-class IncrementalBenchmarks {
+class IncrementalBenchmarks extends BenchmarksWithParallelMap {
   @Param(Array("6"))
   var depth: Int = _
 
@@ -22,8 +23,6 @@ class IncrementalBenchmarks {
 
   @Param(Array("true", "false"))
   var mutableEGraph: Boolean = _
-
-  private val parallelMap: ParallelMap = ParallelMap.sequential
 
   private var cachedTerms: Seq[ArithExpr] = _
 
